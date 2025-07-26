f74babb353f6dd9e4f4e67941d7f866e480d6d04
ThiagoVituka <tvsvig@gmail.com>
2025-07-25 21:48:51 -0300

    Add files via upload

---
+++ Atividades.py
@@ -0,0 +1,46 @@
+## ATIVIDADES:
+
+### 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+### 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+### 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+
+### 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+
+### 5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
+
+
+
+# atividade 1
+
+# numero1 = float(input('Digite um numero: '))
+# potencia = numero1/4
+# print (potencia)
+
+
+# atividade 2
+# nome = 'Thiago'
+# sobrenome = 'Viturino'
+# print(nome,sobrenome)
+
+
+# atividade 3
+# numero1 = int(input('Digite um numero'))
+# numero2 = int(input('Digite um numero'))
+# numero = (numero1, numero2)
+# print (numero)
+
+
+#atividade 4
+# curso = 'Python'
+# curso1 = 30
+# print (curso, curso1)
+
+# atividde 5
+frase = 'A minha idade de '
+palavra = input('Digite uma palavra')
+print (frase, palavra)
+
+
+
---
+++ Aula4.py
@@ -0,0 +1,63 @@
+# # modulo (%) potencia (**) raiz (**(0.5))
+# #quebra de linha
+
+# print('qualquer texto \n qualquer outro texto')
+# print('''
+#       olá,
+#       como vai?
+#       ''')
+
+# # LINGUAGEM FORÇA A ORGANIZAR O CÓDIO
+# # INDENTANDO
+#variaveis em line
+
+nome = 'Maria'
+print(nome)
+
+# igualdade diferenças atribuição 
+
+print (10 == '10')
+
+nome = 'Teste' 
+
+
+
+# modulo(%) potencia(**) raiz(**(0.5))
+
+
+
+# quebra de linha 
+
+
+print('Olá\nComo vai?')
+
+
+print('''
+Olá,
+Como vai?      
+''')
+
+
+print('Olá')
+print('Como vai?')
+
+
+# LINGUAGEM FORÇA A ORGANIZAR O CÓDIGO
+# INDENTANDO  
+
+
+# variveis em line 
+
+
+idade, endereco, curso  = float(input('nº')), input('rua:'), input('Curso')
+print(idade, endereco, curso)
+
+
+
+# igualdade diferenças atribuição
+
+
+print(10 == '10')
+
+
+nome  = 'Teste'
