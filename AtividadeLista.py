dd59d558ae7ae7a06c82f18edbfd633476c90a15
ThiagoVituka <tvsvig@gmail.com>
2025-07-29 21:41:22 -0300

    Add files via upload

---
+++ AtividadeLista.py
@@ -0,0 +1,101 @@
+valor1 = 4
+valor2 = 6
+valor3 = 2
+
+multiplicação = (valor1 * valor2) / valor3
+
+print ('Resultado', multiplicação)
+
+#==========================================================
+
+n1 = 5
+n2 = 7
+
+soma = (n1 + n2)
+
+print ('Resultado', soma)
+
+#========================================================
+
+n1 = 3
+n2 = 4
+
+potencia = (n1 ** n2)
+
+print ('Resultado', potencia)
+
+#=================================================
+
+n1 = 8
+n2 = 12
+n3 = 15
+
+media = (n1 + n2 + n3) / 3
+
+print ('Resutado', media)
+
+#===================================================
+
+n1 = 10
+n2 = 2
+n3 = 5 
+
+subtração = (n1 - n2) * n3
+
+print ('Resultado', subtração)
+
+#=====================================================
+
+n1 = 27
+n2 = 3
+n3 = 5
+
+divisão = (n1 / n2) + n3
+
+print ('Resultado', divisão	)
+#=====================================================
+
+#Calcule o módulo ( % ) de 17 por 4.
+
+n1 = 17
+n2 = 4
+
+modulo = (n1 % n2)
+
+print ('Resultado ', modulo )
+#==================================================
+
+n1 = 9
+n2 = 3 
+n3 = 2
+
+multiplicação = (n1 * n2) ** n3
+
+print ('Resultado ', multiplicação)
+#==================================================
+
+n1 = 81
+
+raiz = (n1 ** 0.5)
+
+print ('Resultado ', raiz)
+
+#===============================================
+
+n1 = 20
+n2 = 3
+n3 = 4
+
+calculo = (n1) + (n2 * n3)
+print ('Resultado ', calculo )
+#===============================================
+n1 = input (15)
+n2 = input (2)
+n3 = input (7)
+
+calcullo =  (n1 * n2) - n3
+print ('Resultado ', calcullo)
+#==============================================
+cubo = 5 **3
+print (cubo)
+#++++++++++++++++++++++++++++++++++++++++++++++
